2422f6865169a15ac52aed56e965e0e288c6c745
Geoffrey <g.h.garrett13@gmail.com>
2022-03-04 16:14:42 +0200

    Updates to DevOps

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(name='multidoc',
       version='0.0.1',
       description="Automated API Documentation.",
-      long_description="Automated API Documentation for multiple programming languages",
-      author='TODO',
-      author_email='todo@example.org',
-      license='TODO',
+      long_description=("A Python library that automates C++/Pybind11/Python" +
+                        " docstring generation from a single source."),
+      author='geoffreygarrett',
+      author_email='geoffreygarrett99@gmail.com',
+      license='MIT',
       packages=['multidoc'],
       zip_safe=False,
       install_requires=[
@@ -16,5 +17,5 @@ setup(name='multidoc',
           # 'Sphinx',
           # ^^^ Not sure if this is needed on readthedocs.org
           # 'something else?',
-          ],
+      ],
       )
